0c1330d677c438eed697854ba9016ab9f6616c07
Matt Hovey <matt@weirdkid.com>
2023-12-08 23:15:34 -0500

    updates to pom, added test app

--- src/main/java/com/weirdkid/quaqua/test/JFileChooserTestApp.java
+++ src/main/java/com/weirdkid/quaqua/test/JFileChooserTestApp.java
@@ -1,9 +1,10 @@
 package com.weirdkid.quaqua.test;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
 
 public class JFileChooserTestApp {
 
